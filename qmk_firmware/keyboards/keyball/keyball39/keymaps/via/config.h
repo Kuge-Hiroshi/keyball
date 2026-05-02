f2de285c6af3b59dad1906e477a11cb10d066160
Hiroshi <gggn3641@gmail.com>
2026-05-02 13:34:03 +0900

    change layer 8

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 // #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 // #define TAPPING_TERM 250
